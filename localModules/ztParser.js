005f5e3ddba21454feac77c1be3be07bebccb286
matthieuEv <evainmatthieu44@gmail.com>
2023-08-11 16:28:09 +0200

    remove changes on getMovieDetails (), and rename it getPageDatas()

--- localModules/ztParser.js
+++ localModules/ztParser.js
@@ -2,15 +2,12 @@
  * @name ztParser
  * @author Sylicium
  * @date 09/08/2023
- * @version 1.6.0
+ * @version 1.7.0
  * @github https://github.com/Sylicium/zone-telechargement-api/
  */
 
 const axios = require("axios")
 const cheerio = require("cheerio");
-require('dotenv').config();
-
-const moviesDb = process.env.MOVIESDB_API_KEY;
 
 var somef = require('./someFunctions');
 
@@ -152,69 +149,310 @@ class ZoneTelechargementParser {
 
 
 
-    async getMovieDetails(movieTitle) {
-        /*
-        Example:
+    async getPageDatas(movieID) {
+        // A refaire entierement
+
         
-        getMovieDetails:  {
-            adult: false,
-            backdrop_path: '/cyecB7godJ6kNHGONFjUyVN9OX5.jpg',
-            genre_ids: [ 28, 878, 12 ],
-            id: 1726,
-            original_language: 'en',
-            original_title: 'Iron Man',
-            overview: 'After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.',
-            popularity: 76.266,
-            poster_path: '/78lPtwv72eTNqFW9COBYI0dWDJa.jpg',
-            release_date: '2008-04-30',
-            title: 'Iron Man',
-            video: false,
-            vote_average: 7.638,
-            vote_count: 24683
-        }*/
-        try {
-            const response = await axios.get(`https://api.themoviedb.org/3/search/movie`, {
-                params: {
-                    api_key: moviesDb,
-                    query: movieTitle,
-                },
-            });
-    
-            const imgUrl = "https://image.tmdb.org/t/p/w780";
-    
-            const genresListResponse = await axios.get(`https://api.themoviedb.org/3/genre/movie/list`, {
-                params: {
-                    api_key: moviesDb,
-                },
-            });
-            const genresList = genresListResponse.data.genres;
-    
-            const movieDetails = response.data.results[0];
+        // let movieURL = this._getBaseURL() + `?p=film&id=${movieID}` // FILM sans S car page de description de UN seul film
 
-            const genreNames = movieDetails.genre_ids.map(id => {
-                const genre = genresList.find(genre => genre.id === id);
-                return genre ? genre.name : '';
-            });
-    
-            const data = {
-                title: movieDetails.original_title,
-                original_language: movieDetails.original_language,
-                description: movieDetails.overview,
-                poster: imgUrl + movieDetails.poster_path,
-                release_date: movieDetails.release_date,
-                genres: genreNames,
-            };
-    
-            return data;
-        } catch (error) {
-            console.error('Erreur lors de la requête :', error);
-            return null;
-        }
+        // if(!movieURL.startsWith(this._getBaseURL())) return {
+        //     status: false,
+        //     error: `Wrong base URL provided`
+        // }
+
+        // if(this._devMode) console.log("movieURL:",movieURL)
+        
+        // let document = await this._getDOMElementFromURL(movieURL)
+
+        // let corpsElement = (
+        //     document.getElementById("dle-content")
+        //     .getElementsByClassName("base")[0]
+        //     .getElementsByClassName("maincont")[0]
+        //     .getElementsByClassName("corps")[0]
+        // );
+        // let mainElement = (
+        //     corpsElement
+        //     .getElementsByTagName("div")[0]
+        // );
+
+
+        // let otherversions_div = mainElement.getElementsByClassName("otherversions")[0]
+        // let version_list_a = [...otherversions_div.getElementsByTagName("a")]
+        // let versions = version_list_a.map(x => {
+        //         return {
+        //             url: this._getBaseURL() + x.getAttribute("href"),
+        //             quality: x.getElementsByTagName("b")[0].textContent,
+        //             language: this._getMatchingGroups(x.getElementsByTagName("b")[1].textContent)[0],
+        //         }
+        // })
+        // // if(this._devMode) console.log("version1:",otherversions_div)
+        // // if(this._devMode) console.log("version2:",version_list_a)
+        // // if(this._devMode) console.log("version3:",versions)
+        
+        // let center_element = mainElement.getElementsByTagName("center")[0]
+
+
+        // let centerElements = [
+        //     [], [], [], [], [], [], [], [], [], [], [], [], [], []
+        // ]
+
+        // let all_cutsElement_src = [
+        //     `/templates/zone/images/infos_film.png`,
+        //     `/templates/zone/images/synopsis.png`,
+        //     `/templates/zone/images/infos_upload.png`,
+        //     `/templates/zone/images/liens.png`,
+        // ]
+
+        // let currentStep = 0
+
+        // let temp = [...center_element.childNodes]
+        // for(let i in temp) {
+        //     let e = temp[i]
+        //     // if(this._devMode) console.log("e.nodeName",e.nodeName)
+
+        //     if(e.nodeName == "img" && all_cutsElement_src.includes(e.getAttribute("src"))) { currentStep++ }
+        //     // if(this._devMode) console.log(`\n\n\n\n=======\n\n centerElements[${currentStep}]`,centerElements[currentStep])
+        //     centerElements[currentStep].push(e)
+        // }
+        
+        
+        // // =============== FILM INFOS ( centerElements[1] ) ===============
+
+        
+        // let filmInfosElements = [
+        //     [], [], [], [], [], [], [], [], [], [], [], [], [], []
+        // ]
+
+        // let all_cutsElement_src2 = [
+        //     `Origine`,
+        //     `Durée`,
+        //     `Réalisation`,
+        //     `Acteur`,
+        //     `Genre`,
+        //     `Année de production`,
+        //     `Titre original`,
+        //     `Critiques`,
+        //     `Bande annonce`
+        // ]
+
+        // let filmInfos_currentStep = 0
+
+        // let temp2 = [...center_element.childNodes]
+        // for(let i in temp2) {
+        //     let e = temp2[i]
+        //     // if(this._devMode) console.log("e.nodeName",e.nodeName)
+
+        //     if(e.nodeName == "strong" && somef.anyWordInText(e.innerHTML, all_cutsElement_src2) ) { filmInfos_currentStep++ }
+
+        //     filmInfosElements[filmInfos_currentStep].push(e)
+        // }
+
+        // for(let i in filmInfosElements) {
+        //     if(this._devMode) console.log(filmInfosElements[i].map(x => x.outerHTML))
+        // }
+
+        // // if(this._devMode) console.log("centerElements[1]:",centerElements[1].map(x => { return x.outerHTML.trim() }))
+        // // if(this._devMode) console.log("centerElements[1]:",centerElements[1].map(x => { return x.nodeName }))
+
+
+        
+        
+        // let filmInfosElements_mapped = {
+        //     /*"Origine": [],
+        //     "Durée": [],
+        //     "Réalisation": [],
+        //     "Acteur": [],
+        //     "Genre": [],
+        //     "Année de production": [],
+        //     "Titre original": [],
+        //     "Critiques": [],
+        //     "Bande annonce": [],*/
+        // }
+
+        // for(let i in filmInfosElements) {
+        //     let e = filmInfosElements[i]
+        //     let firstStrongText = e.filter(x => { return x.nodeName == "strong"})[0]?.getElementsByTagName("u")[0]?.textContent?.toLowerCase()?.trim() ?? null
+        //     if(!firstStrongText) continue;
+
+        //     let the_key;
+        //     if(firstStrongText.includes("origine")) the_key = "Origine"
+        //     else if(firstStrongText.includes("durée")) the_key = "Durée"
+        //     else if(firstStrongText.includes("réalisation")) the_key = "Réalisation"
+        //     else if(firstStrongText.includes("acteur")) the_key = "Acteur"
+        //     else if(firstStrongText.includes("genre")) the_key = "Genre"
+        //     else if(firstStrongText.includes("année")) the_key = "Année de production"
+        //     else if(firstStrongText.includes("titre")) the_key = "Titre original"
+        //     else if(firstStrongText.includes("critiques")) the_key = "Critiques"
+        //     else if(firstStrongText.includes("bande")) the_key = "Bande annonce"
+        //     filmInfosElements_mapped[the_key] = e
+        // }
+
+
+        // for(let i in [...Object.keys(filmInfosElements_mapped)]) {
+        //     let key = [...Object.keys(filmInfosElements_mapped)][i]
+            
+        //     if(this._devMode) console.log(`filmInfosElements_mapped[${key}]:`,filmInfosElements_mapped[key].map(x => x.outerHTML))
+        // }
+
+
+
+
+        // let getHashtagTextNumber = (elementContainer, n) => {
+        //     return elementContainer.filter(x => { return x.nodeName == "#text"})[n] ?? false
+        // }
+
+        // let movieInfos = {
+        //     name: filmInfosElements[7].filter(x => { return x.nodeName =="#text" })[0].textContent.trim(),
+        //     synopsis: centerElements[2].filter(x => { return x.nodeName == "em" })[0].textContent.trim(),
+        //     fileName: [...corpsElement.getElementsByTagName("center")].filter(x => {
+        //         return (x.getElementsByTagName("font")[0]?.getAttribute("color") == "red")
+        //     })[0]?.textContent.trim() ?? null,
+        //     origin: filmInfosElements_mapped["Origine"] ? (getHashtagTextNumber(filmInfosElements_mapped["Origine"], 0)?.textContent?.trim() ?? null) : null,
+        //     duration: filmInfosElements_mapped["Durée"] ? (getHashtagTextNumber(filmInfosElements_mapped["Durée"], 0)?.textContent?.trim() ?? null) : null,
+        //     director: this._getBaseURL() + encodeURI(centerElements[1].filter(x => { return x.nodeName == "a"})[0].getAttribute("href")),
+        //     actors: filmInfosElements[4].filter(x => { return x.nodeName == "a" }).map(x => {
+        //         return {
+        //             name: x.textContent,
+        //             url: this._getBaseURL() + encodeURI(x.getAttribute("href"))
+        //         }
+        //     }),
+        //     genres: filmInfosElements[5].filter(x => { return x.nodeName == "a" }).map(x => {
+        //         return {
+        //             name: x.textContent,
+        //             url: this._getBaseURL() + encodeURI(x.getAttribute("href"))
+        //         }
+        //     }),
+        //     productionYear: filmInfosElements_mapped["Année de production"] ? (getHashtagTextNumber(filmInfosElements_mapped["Année de production"], 0)?.textContent?.trim() ?? null) : null,
+        //     originalTitle: filmInfosElements_mapped["Titre original"] ? (getHashtagTextNumber(filmInfosElements_mapped["Titre original"], 0)?.textContent?.trim() ?? null) : null,
+        //     review: filmInfosElements_mapped["Critiques"] ? (getHashtagTextNumber(filmInfosElements_mapped["Critiques"], 0)?.textContent?.trim() ?? null) : null,
+        //     trailer: filmInfosElements_mapped["Bande annonce"] ? (filmInfosElements_mapped["Bande annonce"].filter(x => {
+        //         try {
+        //             // if(this._devMode) console.log("x.getAttribute('href'):",x.getElementsByTagName("a")[0].getAttribute("href"))
+        //             return x.getElementsByTagName("a")[0].getAttribute("href").indexOf("allocine") != -1
+        //         } catch(e) {
+        //             return false
+        //         }
+        //     })[0]?.getElementsByTagName("a")[0]?.getAttribute("href")?.trim() ?? null) : null,
+        //     downloadLinks: null,
+        //     streamingLinks: null
+        // }
+
+        // if(this._devMode) console.log("infos:",movieInfos)
+
+
+        // // =============== SYNOPSIS ( centerElements[2] ) ===============
+        
+        // /*
+        // if(this._devMode) console.log("centerElements:",centerElements[2].map(x => { return x.nodeName.trim() }))
+        // if(this._devMode) console.log("centerElements:",centerElements[2].map(x => { return x.outerHTML.trim() }))
+        // if(this._devMode) console.log("tagName:",centerElements[2].filter(x => { return x.nodeName == "em" }))
+        // */
+
+
+        
+        // // =============== LIENS DE TELECHARGEMENT ===============
+
+        
+        // let downloadLinksElements = [
+        //     [], [], [], [], [], [], [], [], [], [], [], [], [], []
+        // ]
+
+        // let all_cutsElement_dl = [
+        //     `Liens De Téléchargement`,
+        //     `Liens De Streaming`
+        // ]
+
+        // let downloadLinks_currentStep = 0
+
+        // let temp3 = [...corpsElement.childNodes]
+        // for(let i in temp3) {
+        //     let e = temp3[i]
+        //     // if(this._devMode) console.log("e.nodeName",e.nodeName)
+
+        //     if(e.nodeName == "h2" && somef.anyWordInText(e.innerHTML, all_cutsElement_dl) ) { console.log("downloadLinks STEP +1"); downloadLinks_currentStep++ }
+
+        //     downloadLinksElements[downloadLinks_currentStep].push(e)
+        // }
+
+        // for(let i in downloadLinksElements) {
+        //     // if(this._devMode) console.log(`downloadLinksElements[${i}]:`, downloadLinksElements[i].map(x => x.outerHTML))
+        // }
+
+        // /*
+        // downloadLinksElements[1] : Liens De téléchargement
+        // downloadLinksElements[2] : Liens De Streaming
+
+        // */
+
+        // // ===================== LIENS DE TELECHARGEMENT only  ========================
+
+
+        // function parseDownloadLinks(downloadLinksElements_number) {
+        //     let onlyDownloadLinksElement = downloadLinksElements_number.filter(x => { return x.getAttribute("id") == "news-id-23557" })[0].getElementsByClassName("postinfo")[0]
+        //     let downloadLinksElements_dlprotect = [
+        //         [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []
+        //     ]
+        //     let downloadLinks_dlprotect_currentStep = 0
+        //     let lastWasBr = false
+
+        //     let temp4 = [...onlyDownloadLinksElement.childNodes]
+        //     for(let i in temp4) {
+        //         let e = temp4[i]
+        //         // if(this._devMode) console.log(".nodeName",e.nodeName)
+        //         if(e.nodeName == "#text") continue;
+
+        //         if(e.nodeName == "br" && !lastWasBr) {
+        //             downloadLinks_dlprotect_currentStep++
+        //             lastWasBr = true
+        //             continue;
+        //         }
+        //         lastWasBr = false                
+        //         downloadLinksElements_dlprotect[downloadLinks_dlprotect_currentStep].push(e)
+        //     }
+
+        //     for(let i in downloadLinksElements_dlprotect) {
+        //         // if(this._devMode) console.log(`downloadLinksElements_dlprotect[${i}]`, downloadLinksElements_dlprotect[i].map(x => x.outerHTML))
+        //     }
+
+
+        //     /*for(let i in downloadLinksElements_dlprotect) {
+        //         if(this._devMode) console.log(`downloadLinksElements_dlprotect[${i}]:`, downloadLinksElements_dlprotect[i].map(x => x.outerHTML))
+        //     }*/
+
+        //     let downloadLinksMapped = downloadLinksElements_dlprotect.filter(x => {
+        //         return x.filter(x => x.nodeName == "b").length == 2
+        //     }).map(x => {
+        //         return {
+        //             service: x.filter(x => x.nodeName == "b")[0].textContent.trim(),
+        //             url: x.filter(x => x.nodeName == "b")[1].getElementsByTagName("a")[0].getAttribute("href").trim(),
+        //         }
+        //     })
+        //     return downloadLinksMapped
+        // }
+
+        // movieInfos.downloadLinks = parseDownloadLinks(downloadLinksElements[1])
+
+        // // ===================== LIENS DE TELECHARGEMENT only  ========================
+
+        // movieInfos.streamingLinks = parseDownloadLinks(downloadLinksElements[2])
+
+        // // ===================== ======================== ========================
+        
+
+
+
+
+
+        // let backPayload = {
+        //     movieInfos: movieInfos,
+        //     otherVersions: versions,
+        // }
+
+        // // console.log("backPayload:",backPayload)
+
+        // return backPayload
+
     }
-    
-    
-}    
-
-let ZT = new ZoneTelechargementParser()
+}
 
 module.exports = new ZoneTelechargementParser()
